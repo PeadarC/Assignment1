c42ccdcb4761c87399aa0e62bc835eb84938ea8b
PeadarC <47325876+PeadarC@users.noreply.github.com>
2020-11-24 21:54:12 +0000

    Update Student.java

--- src/main/java/studentReg2/Student.java
+++ src/main/java/studentReg2/Student.java
@@ -1,28 +1,71 @@
 package studentReg2;
 
-import studentReg2.Course;
-import studentReg2.Module;
+import org.joda.time.DateTime;
+import java.util.ArrayList;
 
 public class Student {
-	
-	  // instance variables
-	  String name;
-	  String age;
-	  String dateOfBirth;
-	  int ID;
-	  String userName;
-	  Course[] courses;
-	  Module[] modules;
 
-	  public Student(String name, String age) {
-		  this.name = name;
-		  this.age = age;
-	  }
+    private String name;
+    private int age;
+    private DateTime DOB;
+    private int ID;
+    private ArrayList<CourseProgramme> registeredCourses = new ArrayList<CourseProgramme>();
+    private ArrayList<Module> registeredModules = new ArrayList<Module>();
 
-	// getUsername method 
-	  public String getUsername(){
-	    this.userName = this.name + this.age;
-	    return this.userName;
-	  }
+
+    public Student(String name, int age, DateTime DOB, int ID) {
+        this.name = name;
+        this.age = age;
+        this.DOB = DOB;
+        this.ID = ID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this ) {
+            return true;
+        }
+        if(!(obj instanceof Student)) {
+            return false;
+        }
+
+        Student student = (Student) obj;
+        return this.name == student.name && this.age == student.age && this.DOB == student.DOB && this.ID == student.ID;
+    }
+
+    public void addCourse(CourseProgramme course) {
+        registeredCourses.add(course);
+    }
+
+    public void addModule(Module module) {
+        registeredModules.add(module);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public DateTime getDOB() {
+        return DOB;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getUsername() {
+        return this.name + this.age;
+    }
+
+    public ArrayList<CourseProgramme> getRegisteredCourses() {
+        return registeredCourses;
+    }
+
+    public ArrayList<Module> getRegisteredModules() {
+        return registeredModules;
+    }
 }
-
